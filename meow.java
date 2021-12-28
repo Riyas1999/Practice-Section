8e0baa829752384cb24b7b593521a633e66e75f1
Riyas1999 <96716342+Riyas1999@users.noreply.github.com>
2021-12-28 21:43:57 +0530

    Add files via upload

---
+++ meow.java
@@ -0,0 +1,44 @@
+public class meow {
+    public static void main(String[] args) {
+        //System.out.print("Meow Meow"); 
+        //System.out.print("Riyas"); 
+
+        
+        //int a = 5; // any negative and positive value but it can't be decimal
+        //int b = 2;
+        //System.out.println(a % b); // remainder is zero */
+
+       // String m = "Meow";
+        //String a = "Riyas";
+        //char ch = 'a';
+       // System.out.println(a);
+
+        // double d = 1.09;
+        //System.out.println(d + 2.91);
+
+        //boolean bool = true; // true/false conditional statements
+        //System.out.println(bool); 
+        //int counter = 0;
+        //while ( counter < 5)
+        {
+            //System.out.println( counter);
+            //counter++;
+        }
+        //System.out.println("Done! meow");
+        //for ( int i = 0; i <= 5; i++)
+        {
+            //System.out.println(i);
+        }
+       
+        
+
+
+
+
+
+
+
+
+
+    }
+}
