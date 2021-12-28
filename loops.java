986db75d4f24434951bac1fa1605ae8aa8c7c12f
Riyas1999 <96716342+Riyas1999@users.noreply.github.com>
2021-12-28 22:22:03 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class loops {
+    public static void main(String[] args) {
+
+        //int counter = 0; 
+        //while(counter < 5) { 
+            //System.out.println(counter); 
+            //counter++;
+        //}
+        //System.out.println("Done! meww");
+       
+
+         //for (int i = 1; i <= 10; i++) {
+            //System.out.println(i);
+        //}
+        //boolean start = true;
+        //while(start) {
+            //System.out.println("abcd");
+            //start = false;
+        } 
+    }
+}
+
